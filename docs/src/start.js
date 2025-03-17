6e9deac706c18b8cca6a02deea3d788705f4e0ce
Cindy626 <cindy062689@gmail.com>
2025-03-17 17:29:42 +0000

    Update start.js

--- docs/src/start.js
+++ docs/src/start.js
@@ -1,10 +1,20 @@
 let bgImg;
+let bgGame;
 let angelWords;
 let titleY; // 標題 Y 軸位置
 let angle = 0; // 角度用來計算彈跳幅度
-let playNormal;
-let playHover;
-let playX, playY, playWidth, playHeight;
+
+
+//let playNormal;
+//let playHover;
+let playX, playY;
+let playWidth=100
+let playHeight=40;
+
+let buttonX, buttonY;
+let buttonWidth = 150;
+let buttonHeight = 50;
+
 let pageState=0;
 
 let challengeWords;
@@ -17,9 +27,10 @@ let selectedChallenge = -1;
 
 function preload() {
   bgImg = loadImage('/images/bg.png');
+  bgGame = loadImage('/images/GameBackground.jpg');
   angelWords = loadImage('/images/upup.png');
-  playNormal = loadImage('/images/playorange.png');
-  playHover = loadImage('/images/playgreen.png'); 
+  /*playNormal = loadImage('/images/playorange.png');
+  playHover = loadImage('/images/playgreen.png'); */
   challengeWords=loadImage('/images/selectchallenge.png');
   simple = loadImage('/images/simple1.png');
   simpleHover = loadImage('/images/simple2.png');
@@ -32,11 +43,21 @@ function preload() {
 function setup() {
   createCanvas(800, 600);
   
-  titleY = 0; // 設定標題初始位置
-  playWidth = playNormal.width; // 設定按鈕寬度
-  playHeight = playNormal.height / 1.5; 
-  playX = width / 2 - playWidth / 2;
-  playY = height - playHeight - 100;
+  textAlign(CENTER, CENTER);
+  textFont("Comic Sans MS");
+  textSize(50);
+  
+  titleY = -20; // 設定標題初始位置
+  /*playWidth = playNormal.width; // 設定按鈕寬度
+  playHeight = playNormal.height / 1.5; */
+  
+  // 設定 PLAY 文字的中心位置
+  playX = width /2;
+  playY = height - 150;
+  
+  
+  
+  
   
   simpleWidth = simple.width; // 設定按鈕寬度
   simpleHeight = simple.height ; 
@@ -48,26 +69,30 @@ function draw() {
   if (pageState == 0) { 
     image(bgImg, 0, 0, width, height);
     
-    let bounce = sin(angle) * 10; // 讓標題上下移動 10 像素範圍
+    let bounce = sin(angle) * 20; // 讓標題上下移動 10 像素範圍
     angle += 0.05; // 控制動畫速度
     
     image(angelWords, 90, titleY + bounce, angelWords.width/0.7, angelWords.height/0.7); 
 
-    let isPlayHover = mouseX > playX && mouseX < playX + playWidth && mouseY > playY && mouseY < playY + playHeight;
+    /*let isPlayHover = mouseX > playX && mouseX < playX + playWidth && mouseY > playY && mouseY < playY + playHeight;*/
+    // 檢查滑鼠是否在 PLAY 文字範圍內
+    let isPlayHover = mouseX > playX - playWidth / 2 && mouseX < playX + playWidth / 2 &&
+                      mouseY > playY - playHeight
+  +10/ 2 && mouseY < playY + playHeight / 2;
+
+    // 改變文字顏色（滑鼠懸停變橘色）
+    fill(isPlayHover ? '#DFA0B2' : '#0BCBB8'); 
+    text("PLAY", playX, playY);
+  }
     
-    
-    if (isPlayHover) {
-      image(playHover, playX, playY, playWidth, playHeight);
-    } else {
-      image(playNormal, playX, playY, playWidth, playHeight);
-    }
-  }else if (pageState == 1) {
+
+    else if (pageState == 1) {
     let isSimpleHover = mouseX > simpleX+120 && mouseX < simpleX + simpleWidth-80 && mouseY > simpleY+150 && mouseY < simpleY + simpleHeight-150;
     let isMediumHover = mouseX > simpleX+120 && mouseX < simpleX + simpleWidth-80 && mouseY > simpleY+150+140 && mouseY < simpleY + simpleHeight-150+140;
     
     let isHardHover = mouseX > simpleX+120 && mouseX < simpleX + simpleWidth-80 && mouseY > simpleY+150+140+140 && mouseY < simpleY + simpleHeight-150+140+140;
 
-   image(bgImg, 0, 0, width, height);
+   image(bgGame, 0, 0, width, height);
    image(challengeWords, 0, -80, challengeWords.width/0.8, challengeWords.height/1);
     
     image(simple, 180, 10,  simple.width,  simple.height/1);
@@ -98,11 +123,11 @@ function draw() {
 }
 
 function mousePressed() {
-  if (pageState == 0 &&
-    mouseX > playX && mouseX < playX + playWidth &&
-    mouseY > playY && mouseY < playY + playHeight) 
-  {
-    pageState = 1; 
+  let isPlayHover = mouseX > playX - playWidth / 2 && mouseX < playX + playWidth / 2 &&
+                    mouseY > playY - playHeight / 2 && mouseY < playY + playHeight / 2;
+
+  if (pageState == 0 && isPlayHover) {
+    pageState = 1;
   }   else if (pageState == 1) {
     if (mouseX > 300 && mouseX < 300 + simple.width && mouseY > 100 && mouseY < 100 + simple.height/1.5) {
       selectedChallenge = 0;
